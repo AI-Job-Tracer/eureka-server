edc03c6e5e9123918ff83cac46129e0ef70ba6a3
Uday Annapureddy <uday61543@gmail.com>
2026-02-21 19:39:35 +0530

    initial setup for eureka

---
+++ src/test/java/com/aijobtracker/eureka_serverc/EurekaServercApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aijobtracker.eureka_serverc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServercApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
